74845b86cd2b0c6fb36b4acd1020741d9549c6a6
Ryan Saunders <ryan@kc8zyv.com>
2019-06-20 11:23:40 -0400

    testing user profile changes

--- .firefox.user.js
+++ .firefox.user.js
@@ -1,5 +1,10 @@
-/* Firefox Preferences */
+/* firefox preferences */
 
 
-/* Startup */
+/* startup */
+
+/* don't check if we are the default browser */
 user_pref("browser.shell.checkDefaultBrowser", false);
+
+/* set the startup page to a blank page */
+user_pref("browser.startup.page", 0);
